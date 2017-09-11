db0bb244e933f57cc815a48e59c8350b08b6dc03
Waddlesworth <sirwaddlesworth06@gmail.com>
2017-09-11 21:52:27 +1000

    add node list

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Container from 'samsio/Container';
+import NodeStore from '../stores/nodestore';
+
+class NodeItem extends React.Component {
+    render() {
+        return <li>{this.props.node.system}</li>
+    }
+}
+
+class NodeList extends React.Component {
+    render() {
+        return <ul>{this.props.nodes.map(v => (<NodeItem node={v} />))}</ul>
+    }
+}
 
 class Application extends React.Component {
     render() {
-        return <div></div>;
+        return <Container store={NodeStore}><NodeList /></Container>;
     }
 }
 
