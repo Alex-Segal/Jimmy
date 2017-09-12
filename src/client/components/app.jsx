81351c48fee2fa150cd511e12bacd687ffa84532
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-09-12 17:19:05 +1000

    took waaaay too long to get this far

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import ReactART from 'react-art';
 import Container from 'samsio/Container';
 import NodeStore from '../stores/nodestore';
 
 class NodeItem extends React.Component {
     render() {
-        return <li>{this.props.node.system}</li>
+        const fontStyle = {
+            fontSize: '12',
+            fontFamily: 'Verdana',
+            fontStyle: '#000',
+        };
+        return <ReactART.Text x={50} y={50} alignment="left" font={fontStyle} fill="#000">test</ReactART.Text>;
     }
 }
 
 class NodeList extends React.Component {
     render() {
-        return <ul>{this.props.nodes.map(v => (<NodeItem node={v} />))}</ul>
+        return <ReactART.Surface width={700} height={400}>{this.props.nodes.map((v, i) => (<NodeItem node={v} key={i} />))}</ReactART.Surface>;
     }
 }
 
