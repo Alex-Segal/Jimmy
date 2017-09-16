d63fb07c1e63507eadcab551621351d3d116139f
Waddlesworth <sirwaddlesworth06@gmail.com>
2017-09-16 12:23:55 +1000

    fixed rename box

--- src/client/components/rename.jsx
+++ src/client/components/rename.jsx
@@ -20,8 +20,8 @@ class RenameBox extends React.Component {
         var node = this.props.nodes.filter(v => v.id === this.props.selectedNode);
         if (node.length <= 0) return false;
         node = node[0];
-
-        return <input style={{left: node.pos.x + this.props.panoffset.x, top: node.pos.y + this.props.panoffset.y}} value={this.state.nickname} ref={input => {this.renameBox = input;}} onChange={this.handleChange.bind(this)} onKeyUp={this.handleKeyUp.bind(this)}/>;
+        var point = this.props.transform.inversePoint(node.pos.x, node.pos.y);
+        return <input style={{left: point.x, top: point.y}} value={this.state.nickname} ref={input => {this.renameBox = input;}} onChange={this.handleChange.bind(this)} onKeyUp={this.handleKeyUp.bind(this)}/>;
     }
 
     handleChange(e) {
