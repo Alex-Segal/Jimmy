33955a8d4caa59af9408b14a73f17f68adc7653b
Waddlesworth <sirwaddlesworth06@gmail.com>
2017-09-16 12:27:07 +1000

    wrong point, ignore that

--- src/client/components/rename.jsx
+++ src/client/components/rename.jsx
@@ -20,7 +20,7 @@ class RenameBox extends React.Component {
         var node = this.props.nodes.filter(v => v.id === this.props.selectedNode);
         if (node.length <= 0) return false;
         node = node[0];
-        var point = this.props.transform.inversePoint(node.pos.x, node.pos.y);
+        var point = this.props.transform.point(node.pos.x, node.pos.y);
         return <input style={{left: point.x, top: point.y}} value={this.state.nickname} ref={input => {this.renameBox = input;}} onChange={this.handleChange.bind(this)} onKeyUp={this.handleKeyUp.bind(this)}/>;
     }
 
