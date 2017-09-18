8253b24f19ebf5e9b1ef9398f11afa7c33f20003
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-09-18 12:20:48 +1000

    added system hover

--- src/client/components/detail.jsx
+++ src/client/components/detail.jsx
@@ -60,7 +60,7 @@ class WormholeDetail extends React.Component {
 
 class RouteItemBox extends React.Component {
     render() {
-        return <div className="wormhole-route-box" style={{color: CLASS_COLOURS[this.props.system.class]}}>
+        return <div className="wormhole-route-box" style={{color: CLASS_COLOURS[this.props.system.class]}} data-col={CLASS_COLOURS[this.props.system.class]} data-name={this.props.system.system}>
             {this.props.system.class}
         </div>;
     }
