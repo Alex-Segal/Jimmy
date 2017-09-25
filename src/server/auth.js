a6243b02e03277f2bb31a2b1f80d22c092d8a0ac
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-09-25 15:26:05 +1000

    add auth faul

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 
 function RefreshConnection(key) {
+    if (key == false) throw "no auth";
     return fetch("http://localhost:8091/character/access?key=" + key, {
         method: 'GET',
     }).then(r => r.json());
