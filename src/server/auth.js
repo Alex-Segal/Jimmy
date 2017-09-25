14e15630f08c35290360d69f9d58aa04385c9b65
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-09-25 16:28:40 +1000

    rejected promise

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 function RefreshConnection(key) {
-    if (key == false) throw "no auth";
+    if (key == false) return Promise.reject('no auth');
     return fetch("http://localhost:8091/character/access?key=" + key, {
         method: 'GET',
     }).then(r => r.json());
