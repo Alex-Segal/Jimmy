06219e49eecb0209a821348a5e0bf89ca66c4f4a
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-09-25 16:24:26 +1000

    ignore ping

--- src/server/main.js
+++ src/server/main.js
@@ -3,5 +3,6 @@ import GetCurrentNodes from './nodes';
 import {CharacterPing} from './characters';
 
 AddRequest('ping', function(data) {
+    if (!data.key) return false;
     return CharacterPing(data.key);
 });
