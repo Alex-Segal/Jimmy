fab76f668d952caec03ba6396900d2d6c3ec36ab
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-10-04 17:26:19 +1100

    add killboard tracking

--- src/server/main.js
+++ src/server/main.js
@@ -6,3 +6,19 @@ AddRequest('ping', function(data) {
     if (!data.key) return false;
     return CharacterPing(data.key);
 });
+
+import {AddKBNotify} from './zkill';
+import {GetNodeByID} from './nodes';
+import fetch from 'node-fetch';
+
+AddKBNotify(function(data) {
+    var system = GetNodeByID(data.killmail.solar_system_id);
+    if (!system) return;
+    fetch("http://localhost:8091/kbs/kb", {
+        method: 'POST',
+        body: JSON.stringify({
+            system: system,
+            kb: data,
+        });
+    });
+});
---
+++ src/server/zkill.js
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch';
+import {AddRequest} from './ws';
+
+var LOCAL_KBS = [];
+const NotifyKBs = [];
+
+function GetNewKB() {
+    return fetch("https://redisq.zkillboard.com/listen.php", {
+        headers: {
+            'Accept': 'application/json',
+        },
+        method: 'GET',
+    }).then(r => r.json()).then(function(data) {
+        if (!data.package) return;
+        LOCAL_KBS.push(data.package);
+        NotifyKBs.forEach(v => v(data.package));
+    });
+}
+
+async function KBLoop() {
+    while(true) {
+        await GetNewKB();
+    }
+}
+
+KBLoop();
+
+function AddKBNotify(callable) {
+    NotifyKBs.push(callable);
+}
+
+export {AddKBNotify};
+
+setInterval(function() {
+    LOCAL_KBS = LOCAL_KBS.filter(function(v) {
+        var time = (new Date(v.killmail.killmail_time)).getTime();
+        return time > Date.now() - (24*60*60*1000);
+    });
+}, 86400000);
+
+function GetKBsSystem(system_id) {
+    return LOCAL_KBS.filter(v => v.killmail.solar_system_id == system_id);
+}
+
+export {GetKBsSystem};
+
+AddRequest('get_killboard', function(data) {
+    return GetKBsSystem(data.system);
+});
